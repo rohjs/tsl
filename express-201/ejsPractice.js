00bf4d669491002baf0d416d8054b946c203d71e
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-07-27 13:21:47 +0900

    Practice ejs

--- express-201/ejsPractice.js
+++ express-201/ejsPractice.js
@@ -13,8 +13,31 @@ app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'pages'))
 
+
+function validateUser (req, res, next) {
+  const rand = Math.random() * 1
+  res.locals.validated = rand < 0.5
+  next()
+}
+
+app.use(validateUser)
+
 app.get('/', (req, res, next) => {
-  res.render('index', { msg: 'hello' })
+  // The data in the 2nd arg is going to be appended to res.locals
+  res.render('index', {
+    msg: 'Success',
+    msg2: 'Failed',
+    // HTML came from the DB and we want to render
+    htmlMsg: `<p>html paragraph</p>`
+  })
+})
+
+app.get('/home', (req, res, next) => {
+  res.render('home')
+})
+
+app.get('/about', (req, res, next) => {
+  res.render('about')
 })
 
 app.listen(3000)
